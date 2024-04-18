4d7b7ed278396b31117ec26ffed84a53ce4d4c9c
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-04-18 18:57:50 +0530

    Added redux to the blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-
+import { useEffect,useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/Auth'
+import { login,logout } from './store/authslice'
+import { Footer, Header } from './components'
 
 function App() {
   
- console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-     <h1>A Blog App Using Appwrite</h1>
-    </>
-  )
+ const [loading,SetLoading]=useState(true)
+ const dispatch= useDispatch()
+ useEffect(()=>{
+  authService.getCurrentuser()
+  .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>SetLoading(false))
+  
+ },[])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>    
+      </div>
+    </div>
+):null
 }
 
 export default App
